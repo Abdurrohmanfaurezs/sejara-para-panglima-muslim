0bcd9458f2ac0a2b2abb07547763c9cf262fd979
fauzanfaurezs <fauzanfaurezs@MacBook-Pro-fauzanfaurezs.local>
2018-11-05 14:49:09 +0700

    Initial Commit

---
+++ sejara para panglima muslim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sejara para panglima muslim
+//
+//  Created by fauzanfaurezs on 05/11/2018.
+//  Copyright © 2018 fauzanfaurezs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
